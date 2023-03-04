b21dd06503f4df1abaea17d01bccbc9d68aae7d8
ralphbetta <ralphbetta@gmail.com>
2023-03-04 17:35:22 +0100

    completed the brand section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Brands from "./components/Brands";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -6,6 +7,7 @@ function App() {
     <div className=" bg-slate-900">
       <Navbar/>
       <Hero/>
+      <Brands/>
     </div>
   );
 }
---
+++ src/components/Brands.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { brands } from '../data'
+
+const Brands = () => {
+  return (
+    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 bg-slate-800 px-10 justify-center items-center'>
+        {brands.map((data,index)=>(
+        <div className={index === 4 ?'flex mx-auto col-span-2 sm:col-span-1 py-2':'flex mx-auto py-2'}><img src={data.img} alt="" /></div>
+        ))}
+    </div>
+  )
+}
+
+export default Brands

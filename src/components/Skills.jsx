8ae931efe3605221ff6f09ac6b7339e9fb19dc18
ralphbetta <ralphbetta@gmail.com>
2023-03-06 21:49:46 +0100

    completed on view scaling animation

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { skills } from '../data'
+import { motion } from 'framer-motion'
 
 const Skills = () => {
   return (
     <div className='grid grid-cols-2 sm:grid-cols-4 md:grid-cols-8 px-10 bg-neutral-900'>
         {
            skills.map((data, index)=>(
-            <img key={index} className=' w-full p-4' src={data.image} alt="" />
+           <motion.div 
+           initial={{scale:0}}
+           whileInView={{scale:1}}
+           transition={{duration: 0.5}}
+           >
+             <img key={index} className=' w-full p-4' src={data.image} alt="" />
+           </motion.div>
            )) 
         }
     </div>
